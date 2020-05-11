ba6b998c8a23825a2fef00eb72fa3e2c285de10d
DanZZZeDanZZZeDanZZZe <jyjyjyj3@gmail.com>
2020-05-11 22:30:20 +0300

    add babel plugin

--- webpack.config.js
+++ webpack.config.js
@@ -123,6 +123,9 @@ module.exports = {
           options: {
             presets: [
               '@babel/preset-env'
+            ],
+            plugins: [
+              '@babel/plugin-proposal-class-properties'
             ]
           } 
         }
