765bc5bc7144d8fce10764e6e92814977cdb30d2
DanZZZeDanZZZeDanZZZe <jyjyjyj3@gmail.com>
2020-05-11 23:01:13 +0300

    add react

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,8 @@ import Post from '@models/Post'
 import json from './assets/json'
 import xml from './assets/data.xml'
 import WebapckLogo from './assets/webpack-logo'
+import React from 'react'
+import {render} from 'react-dom'
 import './styles/styles.css'
 import './styles/less.less'
 import './styles/sass.sass'
@@ -13,7 +15,25 @@ const post = new Post('Webpack Post title', WebapckLogo)
 
 $('pre').html(post.toString())
 
-console.log('Post to String', post.toString())
+const App = () => (
+<div className="container">
+  <h1>Webpack Course</h1>
+  <hr />
+  <div className="logo" />
+  <hr />
+  <pre />
+  <hr />
+  <div className="box">
+    <h2>Less</h2>
+  </div>
+  <div className="card">
+    <h2>Sass</h2>
+  </div>
+</div>
+)
 
+render(<App />, document.getElementById('app'))
+
+console.log('Post to String', post.toString())
 console.log('JSON:', json)
 console.log('xml:', xml)
